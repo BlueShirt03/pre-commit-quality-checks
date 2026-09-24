7253d2bc1eb7c889fc99f533dbbce20b42ac1690
BlueShirt03 <crcastillo2014@gmail.com>
2026-09-23 21:49:16 -0500

    Added a new function and unit test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
-from src.calculator import add, subtract
+from src.calculator import add, subtract, multiple
 
 
 def test_add():
@@ -7,3 +7,7 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+def test_multiple():
+    assert multiple(2, 5) == 10
